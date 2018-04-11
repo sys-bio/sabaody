e59558599e162d5d500a2d9d9161005cc76c455d
0u812 <0u812@users.noreply.github.com>
2018-04-10 21:41:06 -0700

    initl commit

---
+++ sabaody/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import print_function, division, absolute_import
+
+__version__ = '0.1.0'
+
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(name='sabaody',
+      version='0.1.0',
+      description='Distributed Island Model',
+      author='Shaik Asifullah, J. Kyle Medley',
+      packages=['sabaody'])
