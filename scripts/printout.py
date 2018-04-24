b4c2609f9fa3d1d886bf71da49c77eddfb797729
Your Name <you@example.com>
2018-04-23 21:19:29 -0700

    try var types logging

--- scripts/printout.py
+++ scripts/printout.py
@@ -2,6 +2,7 @@ from pyspark import SparkContext, SparkConf
 conf = SparkConf().setAppName("printout")
 sc = SparkContext(conf=conf)
 sc.setLogLevel("ERROR")
+import logging
 from pprint import PrettyPrinter
 pp = PrettyPrinter(2)
 
@@ -22,6 +23,13 @@ def spark_method(x):
     #sleep(30)
     import socket
     hostname = socket.gethostname()
+    ## https://stackoverflow.com/questions/25407550/how-do-i-log-from-my-python-spark-script
+    #log4jLogger = sc._jvm.org.apache.log4j
+    #l = log4jLogger.LogManager.getLogger(__name__)
+    hoststr = 'HOST: {} **********'.format(hostname)
+    l = logging.getLogger('worker')
+    l.warn("pyspark script logger initialized")
+    l.warn(hoststr)
     #return hostname
     #return [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
     import sys
