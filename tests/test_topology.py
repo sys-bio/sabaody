5e966f5f53c20a59bc0bc2722213da5a84f032bf
0u812 <0u812@users.noreply.github.com>
2018-06-01 10:16:15 -0700

    test island subsets

--- tests/test_topology.py
+++ tests/test_topology.py
@@ -24,3 +24,4 @@ def test_one_way_ring():
         assert len(t.outgoing_islands(id)) == len(t.outgoing_ids(id)) == 1
         # outgoing node should be in neighbors
         assert frozenset(t.outgoing_ids(id)) < frozenset(t.neighbor_ids(id))
+        assert frozenset(t.outgoing_islands(id)) < frozenset(t.neighbor_islands(id))
