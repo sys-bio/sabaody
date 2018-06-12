01e6ca0ccf2dc1af0b34f8ebc967ef471fca4417
0u812 <0u812@users.noreply.github.com>
2018-06-11 18:33:29 -0700

    always run nbs

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,10 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'nbsphinx',
+    'sphinx.ext.mathjax',
 ]
+
+nbsphinx_execute = 'always'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -67,7 +70,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
