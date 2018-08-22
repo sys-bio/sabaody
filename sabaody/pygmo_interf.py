90997cfc0126109ca10d617d7445f071a4e47c1e
0u812 <0u812@users.noreply.github.com>
2018-08-22 11:41:15 -0700

    fix missing syms

--- sabaody/pygmo_interf.py
+++ sabaody/pygmo_interf.py
@@ -69,12 +69,12 @@ def run_island(island, topology, migrator):
         #migrator.send_migrants(island.id,i,topology,generation=x)
         #deltas,src_ids = migrator.receive_migrants(island.id,i,topology,generation=x)
 
-        migration_log.append((float(pop.champion_f[0]),deltas,src_ids))
+        migration_log.append((float(i.get_population().champion_f[0]),deltas,src_ids))
 
     #import socket
     #hostname = socket.gethostname()
     #ip = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
-    return (ip, hostname, island.id, migration_log, i.get_population().problem.get_fevals())
+    #return (ip, hostname, island.id, migration_log, i.get_population().problem.get_fevals())
     return i.get_population().problem.get_fevals()
 
 class Archipelago:
