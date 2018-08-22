72f3a01fc41f796c571ef300cb55364a96fd91d9
0u812 <0u812@users.noreply.github.com>
2018-08-21 18:05:58 -0700

    test for getting most recent migrants

--- tests/test_kafka_migration_service.py
+++ tests/test_kafka_migration_service.py
@@ -15,3 +15,15 @@ def test_migrate():
     assert array_equal(migrants, array([[1,2,3,4]]))
     assert array_equal(fitness, array([[0.]]))
     assert src_ids == ['from-island']
+
+def test_most_recent_migrants():
+    from sabaody.kafka_migration_service import KafkaMigrator, KafkaBuilder
+    migrator = KafkaMigrator(None, None, KafkaBuilder('localhost', 9092))
+
+    for k in range(10):
+        migrator.migrate('to-island', array([k]), array([0.]), 'from-island')
+
+    migrants,fitness,src_ids = migrator.welcome("to-island",n=5)
+
+    assert array_equal(migrants, array([[9,8,7,6,5]]))
+    assert src_ids == ['from-island']*5
