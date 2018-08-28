13ad02f45ad2c9896f74ee0a2390a07e49ce6902
0u812 <0u812@users.noreply.github.com>
2018-08-28 15:48:17 -0700

    move argument parsing logic into b2setup

--- sabaody/scripts/b2/b2-driver.py
+++ sabaody/scripts/b2/b2-driver.py
@@ -1,79 +1,14 @@
 # Driver code for B2 parameter fitting problem.
 from __future__ import print_function, division, absolute_import
 
-import argparse
-from os.path import join, abspath, dirname, realpath
-from re import match
-from pyspark import SparkContext, SparkConf
+from b2setup import B2Configuration, run_b2_islands
 
-parser = argparse.ArgumentParser(description='Run the B2 problem.')
-parser.add_argument('host', metavar='hostname',
-                    help='The hostname of the master node of the spark cluster with optional port, e.g. localhost:7077')
-parser.add_argument('--topology', required=True,
-                    choices = ['ring', 'bidir-ring', 'one-way-ring'],
-                    help='The topology to use')
-parser.add_argument('--migration', required=True,
-                    choices = ['none', 'null',
-                      'central', 'central-migrator',
-                      'kafka', 'kafka-migrator',
-                     ],
-                    help='The migration scheme to use')
-parser.add_argument('--num-islands', type=int, required=True,
-                    help='The migration scheme to use')
-args = parser.parse_args()
-if not match(r'[^: ](:[\d]+)?', args.host):
-    raise RuntimeError('Expected host name to be either a name or name:port')
-if not ':' in args.host:
-    hostname = args.host
-    port = 7077
-else:
-    hostname,port = args.host.split(':')
-topology_name = args.topology
-migrator_name = args.migration
-n_islands = args.num_islands
-print('Connecting to spark master {}:{}'.format(hostname,port))
+config = B2Configuration.from_cmdline_args()
 
-conf = SparkConf().setAppName("b2-driver")
-conf.setMaster('spark://{}:{}'.format(hostname,port))
-conf.set('spark.driver.memory', '1g')
-#conf.set('spark.executor.memory', '2g')
-#conf.set('spark.executor.cores', '4')
-#conf.set('spark.cores.max', '40')
-
-script_dir = dirname(realpath(__file__))
-
-# set files to be copied to the cwd of each executor
-spark_files = ','.join(join(script_dir,p) for p in [
-    abspath(join('..','..','..','sbml','b2.xml')),
-    ])
-py_files = ','.join(join(script_dir,p) for p in [
-    'data.py',
-    'b2problem.py',
-    'params.py',
-    'b2setup.py',
-    ])
-
-conf.set('spark.files', ','.join([spark_files,py_files]))
-# set py files
-conf.set('spark.submit.pyFiles', py_files)
-conf.set('spark.logConf', True)
-
-sc = SparkContext(conf=conf)
-
-island_size = 10
-migrant_pool_size = 5
+config.island_size = 10
+config.migrant_pool_size = 5
 from sabaody.migration import BestSPolicy, FairRPolicy
-selection_policy = BestSPolicy(migration_rate=migrant_pool_size)
-replacement_policy = FairRPolicy()
+config.selection_policy = BestSPolicy(migration_rate=config.migrant_pool_size)
+config.replacement_policy = FairRPolicy()
 
-from b2setup import run_b2_islands
-run_b2_islands(
-    spark_context=sc,
-    topology_name=topology_name,
-    migrator_name=migrator_name,
-    island_size=island_size,
-    n_islands=n_islands,
-    migrant_pool_size=migrant_pool_size,
-    selection_policy=selection_policy,
-    replacement_policy=replacement_policy,
-    )
+run_b2_islands(config)
--- sabaody/scripts/b2/b2setup.py
+++ sabaody/scripts/b2/b2setup.py
@@ -3,8 +3,9 @@ from __future__ import print_function, division, absolute_import
 from params import getDefaultParamValues
 
 from sabaody.problem_setup import ProblemSetup
-
 from sabaody import Archipelago
+
+from pyspark import SparkContext, SparkConf
 
 # some refs:
 #https://developerzen.com/best-practices-writing-production-grade-pyspark-jobs-cb688ac4d20f
@@ -31,8 +32,97 @@ class B2Run(ProblemSetup):
         self.initial_score = p.evaluate(getDefaultParamValues())
         print('Initial score: {}'.format(self.initial_score))
 
-def run_b2_islands(spark_context, topology_name, migrator_name, island_size, n_islands, migrant_pool_size, selection_policy, replacement_policy):
+class B2Configuration:
+    '''
+    A class to handle initialization of the configuration of the problem run
+    and algorithmic parameters, including the island topology, migration settings,
+    and Spark configs.
+    This class takes a topology name and generates a corresponding topology.
+    '''
+    def _initialize(self, app_name='b2-driver'):
+        '''
+        Sets up the Spark config to bundle all Python scripts and SBML files
+        necessary to run the problem.
+        '''
+        from os.path import join, abspath, dirname, realpath
+        self.spark_conf = SparkConf().setAppName(app_name)
+        self.spark_conf.setMaster('spark://{}:{}'.format(self.hostname,self.port))
+        self.spark_conf.set('spark.driver.memory', '1g')
+        #self.spark_conf.set('spark.executor.memory', '2g')
+        #self.spark_conf.set('spark.executor.cores', '4')
+        #self.spark_conf.set('spark.cores.max', '40')
+
+        script_dir = dirname(realpath(__file__))
+
+        # set files to be copied to the cwd of each executor
+        spark_files = ','.join(join(script_dir,p) for p in [
+            abspath(join('..','..','..','sbml','b2.xml')),
+            ])
+        py_files = ','.join(join(script_dir,p) for p in [
+            'data.py',
+            'b2problem.py',
+            'params.py',
+            'b2setup.py',
+            ])
+
+        self.spark_conf.set('spark.files', ','.join([spark_files,py_files]))
+        # set py files
+        self.spark_conf.set('spark.submit.pyFiles', py_files)
+        self.spark_conf.set('spark.logConf', True)
+
+        # after setting up the Spark config, we can instantiate the Spark context
+        self.spark_context = SparkContext(conf=self.spark_conf)
+
+
+    @classmethod
+    def from_cmdline_args(cls):
+        '''
+        Initializes the run configuration from command line arguments.
+        '''
+        config = cls()
+        import argparse
+        parser = argparse.ArgumentParser(description='Run the B2 problem.')
+        parser.add_argument('host', metavar='hostname',
+                            help='The hostname of the master node of the spark cluster with optional port, e.g. localhost:7077')
+        parser.add_argument('--topology', required=True,
+                            choices = ['ring', 'bidir-ring', 'one-way-ring'],
+                            help='The topology to use')
+        parser.add_argument('--migration', required=True,
+                            choices = ['none', 'null',
+                              'central', 'central-migrator',
+                              'kafka', 'kafka-migrator',
+                            ],
+                            help='The migration scheme to use')
+        parser.add_argument('--num-islands', type=int, required=True,
+                            help='The migration scheme to use')
+        args = parser.parse_args()
+        from re import match
+        if not match(r'[^: ](:[\d]+)?', args.host):
+            raise RuntimeError('Expected host name to be either a name or name:port')
+        if not ':' in args.host:
+            config.hostname = args.host
+            config.port = 7077
+        else:
+            config.hostname,config.port = args.host.split(':')
+        config.topology_name = args.topology
+        config.migrator_name = args.migration
+        config.n_islands = args.num_islands
+
+        config._initialize()
+
+        return config
+
+def run_b2_islands(config):
     with B2Run('luna', 11211) as run:
+        spark_context = config.spark_context
+        topology_name = config.topology_name
+        migrator_name = config.migrator_name
+        island_size = config.island_size
+        n_islands = config.n_islands
+        migrant_pool_size = config.migrant_pool_size
+        selection_policy = config.selection_policy
+        replacement_policy = config.replacement_policy
+
         from b2problem import make_problem
 
         import arrow
@@ -64,12 +154,12 @@ def run_b2_islands(spark_context, topology_name, migrator_name, island_size, n_i
         if migrator_name == 'central' or migrator_name == 'central-migrator':
             from sabaody.migration_central import CentralMigrator
             # central migrator process must be running
-            migrator = CentralMigrator(selection_policy, replacement_policy, 'http://luna:10100')
+            migrator = CentralMigrator(selection_policy, replacement_policy, 'http://luna:10100') # FIXME: hardcoded
             migrator.defineMigrantPools(a.topology, 133)
         elif migrator_name == 'kafka' or migrator_name == 'kafka-migrator':
             from sabaody.kafka_migration_service import KafkaMigrator, KafkaBuilder
             # Kafka must be running
-            migrator = KafkaMigrator(selection_policy, replacement_policy, KafkaBuilder('luna', 9092))
+            migrator = KafkaMigrator(selection_policy, replacement_policy, KafkaBuilder('luna', 9092)) # FIXME: hardcoded
         else:
             raise RuntimeError('Migration scheme undefined')
         a.set_mc_server(run.mc_host, run.mc_port, run.getNameQualifier())
