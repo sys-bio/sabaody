92d58d57265be98cdaa5ab2135d63a0f1a4b82cf
0u812 <0u812@users.noreply.github.com>
2018-12-15 18:18:30 -0800

    add b4 driver and clean up old driver settings

--- sabaody/scripts/benchmarks/biopredyn/b4/b4-driver.py
+++ sabaody/scripts/benchmarks/biopredyn/b4/b4-driver.py
@@ -3,17 +3,16 @@ from __future__ import print_function, division, absolute_import
 
 from sabaody.scripts.benchmarks.biopredyn.benchsetup import BiopredynConfiguration
 from params import getDefaultParamValues, getUpperBound, getLowerBound
-from b2problem import B2_UDP
-from b2problem_validator import B2Validator_UDP
+from b4problem import B4_UDP
 
 from os.path import dirname, realpath
 
 def get_udp(validation_mode,n):
     if not validation_mode:
-        return B2_UDP(getLowerBound(),getUpperBound())
+        return B4_UDP(getLowerBound(),getUpperBound())
     else:
-        return B2Validator_UDP(getLowerBound(),getUpperBound(),n=n)
+        raise RuntimeError('No validation')
 
-config = BiopredynConfiguration.from_cmdline_args('b2-driver', '../../../../../sbml/b2.xml', dirname(realpath(__file__)), get_udp, getDefaultParamValues)
+config = BiopredynConfiguration.from_cmdline_args('b4-driver', '../../../../../sbml/b4.xml', dirname(realpath(__file__)), get_udp, getDefaultParamValues)
 
 config.run_command(config.command)
