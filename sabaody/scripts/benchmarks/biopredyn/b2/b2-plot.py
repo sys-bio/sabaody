84642a550cefec19184961e358f8c3489b13afc5
0u812 <0u812@users.noreply.github.com>
2018-12-19 15:54:58 -0800

    update best known values for b2

--- sabaody/scripts/benchmarks/biopredyn/b2/b2-plot.py
+++ sabaody/scripts/benchmarks/biopredyn/b2/b2-plot.py
@@ -3,12 +3,12 @@ from __future__ import print_function, division, absolute_import
 # run this in a notebook to plot
 
 from b2problem import B2Problem
-from params import getDefaultParamValues
+from params import getDefaultParamValues, getBestKnownValues
 
 with open('../../../../../sbml/b2.xml') as f:
     sbml = f.read()
 m = B2Problem(sbml)
-m.evaluate(getDefaultParamValues())
-q = 'cpep'
-m.plotQuantity(q)
-print('MSE for {}: {:.3}'.format(q,m.RMSE_quantity(q)))
+# m.evaluate(getBestKnownValues())
+for q in ['cpep', 'cg6p', 'cpyr', 'cf6p', 'cglcex', 'cg1p', 'cpg', 'cfdp', 'cgap']:
+    m.plotQuantity(q, param_values=getBestKnownValues())
+    print('MSE for {}: {:.3}'.format(q,m.RMSE_quantity(q)))
