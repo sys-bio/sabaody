14762b16824591973dde42d561de2d730a9245a2
0u812 <0u812@users.noreply.github.com>
2019-02-23 08:56:31 -0800

    compute average diff for topologies

--- sabaody/scripts/pagmobench-analysis/analysis.py
+++ sabaody/scripts/pagmobench-analysis/analysis.py
@@ -13,6 +13,15 @@ if __name__ == '__main__':
     # print(topology_rank)
     topology_rank_min = topology_rank.min(axis=0,level=1)
     topology_rank_max = topology_rank.max(axis=0,level=1)
+    topology_rank_range = topology_rank_max-topology_rank_min
     # print(topology_rank_min['ActualAvgRounds'])
+    print('Range per connectivity graph:')
+    s = 0.
     for i in topology_rank_min.index:
-        print(i, '{}-{}'.format(int(topology_rank_min['ActualAvgRounds'][i]), int(topology_rank_max['ActualAvgRounds'][i])))
+        print(i, '{}-{}'.format(
+            int(topology_rank_min['ActualAvgRounds'][i]), int(topology_rank_max['ActualAvgRounds'][i])),
+            topology_rank_range['ActualAvgRounds'][i])
+        s += float(topology_rank_range['ActualAvgRounds'][i])
+    mean_range = float(topology_rank_range.mean())
+    print('Average difference: {}'.format(mean_range))
+    print(s/14.)
