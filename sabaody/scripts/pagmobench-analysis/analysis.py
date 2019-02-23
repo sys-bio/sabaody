66565691db38035ebc0d66338d82da9d7c69cee1
0u812 <0u812@users.noreply.github.com>
2019-02-23 08:38:07 -0800

    compute benchmark rank

--- sabaody/scripts/pagmobench-analysis/analysis.py
+++ sabaody/scripts/pagmobench-analysis/analysis.py
@@ -1,11 +1,18 @@
-from pandas import read_csv
+from pandas import read_csv, DataFrame
 
 from os.path import join, dirname, realpath
+from pprint import pprint
 
 if __name__ == '__main__':
     d = read_csv(join(dirname(realpath(__file__)), 'pagmobench.tsv'), sep='\t', header=0)
-    print(d.groupby(('Benchmark','Description'))['ActualAvgRounds'].mean())
-    # for benchmark,bench_data in d.groupby(('Benchmark','Description'))['ActualAvgRounds'].mean():
-        # print(benchmark)
-        # print(tuple(bench_data.keys()))
-        # print(bench_data.groupby().groups)
+    replicate_mean = DataFrame(d.groupby(('Benchmark','Description'))['ActualAvgRounds'].mean())
+    # print(replicate_mean)
+    rank = DataFrame(replicate_mean.groupby('Benchmark').rank())
+    print(rank)
+    # d['Rank'] = d.groupby(('Benchmark','Description'))['ActualAvgRounds'].rank()
+    # d['MinRank'] = d.groupby('Description')['ActualAvgRounds'].min()
+    # d['MaxRank'] = d.groupby('Description')['ActualAvgRounds'].max()
+    # print(d)
+    # print(d.groupby(('Benchmark','Description')))
+    # pprint(d.groupby(('Benchmark','Description'))['ActualAvgRounds'].groups)
+    # print(d.groupby(('Benchmark','Description'))['ActualAvgRounds'].rank())
