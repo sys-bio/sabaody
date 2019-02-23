56f9f8202c38290739f4db97584d7afc564b3e68
0u812 <0u812@users.noreply.github.com>
2019-02-22 18:13:04 -0800

    save progress

--- sabaody/scripts/pagmobench-analysis/analysis.py
+++ sabaody/scripts/pagmobench-analysis/analysis.py
@@ -1,7 +1,11 @@
-from pandas import DataFrame
+from pandas import read_csv
 
 from os.path import join, dirname, realpath
 
 if __name__ == '__main__':
-    d = DataFrame.from_csv(join(dirname(realpath(__file__)), 'pagmobench.tsv'), sep='\t', header=0)
-    print(d)
+    d = read_csv(join(dirname(realpath(__file__)), 'pagmobench.tsv'), sep='\t', header=0)
+    print(d.groupby(('Benchmark','Description'))['ActualAvgRounds'].mean())
+    # for benchmark,bench_data in d.groupby(('Benchmark','Description'))['ActualAvgRounds'].mean():
+        # print(benchmark)
+        # print(tuple(bench_data.keys()))
+        # print(bench_data.groupby().groups)
