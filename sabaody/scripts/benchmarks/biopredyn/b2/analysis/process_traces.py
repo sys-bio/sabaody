2a499d8621264af877eadcd4fa0c1d729573efa3
0u812 <0u812@users.noreply.github.com>
2019-03-11 16:16:44 -0700

    add process traces script for b2

---
+++ sabaody/scripts/benchmarks/biopredyn/b2/analysis/process_traces.py
@@ -0,0 +1,58 @@
+from pandas import read_csv, DataFrame
+
+from os.path import join, dirname, realpath
+from pprint import pprint
+import arrow
+from influxdb import InfluxDBClient
+from ast import literal_eval
+# from numpy import array
+import json
+
+import argparse
+parser = argparse.ArgumentParser(description='Run central migration service.')
+parser.add_argument('tsv',
+                    help='The input tsv.')
+args = parser.parse_args()
+tsv = args.tsv
+
+if __name__ == '__main__':
+    d = read_csv(tsv, sep='\t', header=0)
+
+    replicates = d.groupby(('NumIslands','Description'))
+
+    fastest_time = None
+    for (n_islands, desc), group in replicates:
+        mean_hours = group['Hours'].mean()
+        if fastest_time == None or mean_hours < fastest_time:
+            fastest_time = mean_hours
+
+    summary = DataFrame(columns=['Number of Islands','Connectivity','Algorithm(s)','Hours (Mean)','Hours (SD)','Min. Score (Mean)','Min. Score (SD)','Ratio vs. Fastest','Converged?'])
+    for (n_islands, desc), group in replicates:
+        desc = desc.replace('Bidirectional chain', 'Singleton')
+        topology, algorithm = desc.split(', ')
+
+        mean_hours = group['Hours'].mean()
+        std_hours = group['Hours'].std()
+
+        mean_min_score = group['MinScore'].mean()
+        std_min_score = group['MinScore'].std()
+
+        mean_rounds = group['ActualAvgRounds'].mean()
+        stddev_rounds = group['ActualAvgRounds'].std()
+
+        ratio_vs_fastest = round(mean_hours / fastest_time, 2)
+        converged = bool(mean_rounds < 999)
+
+        summary = summary.append({
+            'Number of Islands': n_islands,
+            'Connectivity': topology,
+            'Algorithm(s)': algorithm,
+            'Hours (Mean)': mean_hours,
+            'Hours (SD)': std_hours,
+            'Min. Score (Mean)': mean_min_score,
+            'Min. Score (SD)': std_min_score,
+            'Ratio vs. Fastest': ratio_vs_fastest,
+            'Converged?': converged,
+        }, ignore_index=True)
+
+    print(summary)
