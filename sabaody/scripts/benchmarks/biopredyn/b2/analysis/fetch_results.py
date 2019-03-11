72f3a71db1ab2db0805e73305c9c12ee1fd52038
0u812 <0u812@users.noreply.github.com>
2019-03-11 14:37:56 -0700

    fetch all traces

--- sabaody/scripts/benchmarks/biopredyn/b2/analysis/fetch_results.py
+++ sabaody/scripts/benchmarks/biopredyn/b2/analysis/fetch_results.py
@@ -2,8 +2,10 @@ from pandas import read_csv, DataFrame
 
 from os.path import join, dirname, realpath
 from pprint import pprint
+import arrow
 from influxdb import InfluxDBClient
-from numpy import array
+from ast import literal_eval
+# from numpy import array
 import json
 
 import argparse
@@ -21,25 +23,32 @@ if __name__ == '__main__':
     for index, row in d.iterrows():
         metric_id = row['MetricId']
 
-    client = InfluxDBClient('luna')
-    tstart = None
-    timepoints_by_island = {}
-    results = client.query('SELECT island_id,best_f,best_x FROM champion', database=metric_id)
-    for result in results:
-        for point in result:
-            t = arrow.get(point['time'])
-            if tstart == None or t < tstart:
-                tstart = t
-            island_id = point['island_id']
-            best_f = literal_eval(point['best_f'])[0]
-            if min_f is None or best_f < min_f:
-                min_f = best_f
-                best_x = literal_eval(point['best_x'])
-            timepoints_by_island.setdefault(island_id, []).append((t.seconds,best_f))
+        tstart = None
+        client = InfluxDBClient('luna')
+        results = client.query('SELECT island_id,best_f,best_x FROM champion', database=metric_id)
+        for result in results:
+            for point in result:
+                t = arrow.get(point['time'])
+                if tstart == None or t < tstart:
+                    tstart = t
 
-    traces = {}
-    for island_id,series in timepoints_by_island.items():
-        traces[island_id] = sorted(series, key=lambda t: t[0])
+        timepoints_by_island = {}
+        min_f = None
+        best_x = None
 
-    with open(join(dirname(realpath(__file__)), 'traces', '.'.join([metric_id,'json'])), 'w') as f:
-        json.dump(traces, f)
+        for result in results:
+            for point in result:
+                t = arrow.get(point['time'])-tstart
+                island_id = point['island_id']
+                best_f = literal_eval(point['best_f'])[0]
+                if min_f is None or best_f < min_f:
+                    min_f = best_f
+                    best_x = literal_eval(point['best_x'])
+                timepoints_by_island.setdefault(island_id, []).append((t.seconds,best_f))
+
+        traces = {}
+        for island_id,series in timepoints_by_island.items():
+            traces[island_id] = sorted(series, key=lambda t: t[0])
+
+        with open(join(dirname(realpath(__file__)), 'traces', '.'.join([metric_id,'json'])), 'w') as f:
+            json.dump(traces, f)
