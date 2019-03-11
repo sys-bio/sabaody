0207f6618f30eab05e057c702147c7fdf28feb7c
0u812 <0u812@users.noreply.github.com>
2019-03-11 13:51:04 -0700

    add b2 analysis

---
+++ sabaody/scripts/benchmarks/biopredyn/b2/analysis/fetch_results.py
@@ -0,0 +1,45 @@
+from pandas import read_csv, DataFrame
+
+from os.path import join, dirname, realpath
+from pprint import pprint
+from influxdb import InfluxDBClient
+from numpy import array
+import json
+
+import argparse
+parser = argparse.ArgumentParser(description='Run central migration service.')
+parser.add_argument('infile',
+                    help='The input tsv.')
+args = parser.parse_args()
+infile = args.infile
+
+if __name__ == '__main__':
+    d = read_csv(infile, sep='\t', header=0)
+    d['Topology'] = tuple(desc.split(', ')[0] for desc in d['Description'])
+    d['Algorithm'] = tuple(desc.split(', ')[1] for desc in d['Description'])
+
+    for index, row in d.iterrows():
+        metric_id = row['MetricId']
+
+    client = InfluxDBClient('luna')
+    tstart = None
+    timepoints_by_island = {}
+    results = client.query('SELECT island_id,best_f,best_x FROM champion', database=metric_id)
+    for result in results:
+        for point in result:
+            t = arrow.get(point['time'])
+            if tstart == None or t < tstart:
+                tstart = t
+            island_id = point['island_id']
+            best_f = literal_eval(point['best_f'])[0]
+            if min_f is None or best_f < min_f:
+                min_f = best_f
+                best_x = literal_eval(point['best_x'])
+            timepoints_by_island.setdefault(island_id, []).append((t.seconds,best_f))
+
+    traces = {}
+    for island_id,series in timepoints_by_island.items():
+        traces[island_id] = sorted(series, key=lambda t: t[0])
+
+    with open(join(dirname(realpath(__file__)), 'traces', '.'.join([metric_id,'json'])), 'w') as f:
+        json.dump(traces, f)
