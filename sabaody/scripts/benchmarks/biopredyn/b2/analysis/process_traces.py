9ab0b2b68dc16f69085a0426c2d4da4554d0556a
0u812 <0u812@users.noreply.github.com>
2019-03-11 16:27:57 -0700

    add convergence, p value

--- sabaody/scripts/benchmarks/biopredyn/b2/analysis/process_traces.py
+++ sabaody/scripts/benchmarks/biopredyn/b2/analysis/process_traces.py
@@ -21,10 +21,12 @@ if __name__ == '__main__':
     replicates = d.groupby(('NumIslands','Description'))
 
     fastest_time = None
+    fastest_hours = None
     for (n_islands, desc), group in replicates:
         mean_hours = group['Hours'].mean()
         if fastest_time == None or mean_hours < fastest_time:
             fastest_time = mean_hours
+            fastest_hours = DataFrame(group['Hours'])
 
     summary = DataFrame(columns=['Number of Islands','Connectivity','Algorithm(s)','Hours (Mean)','Hours (SD)','Min. Score (Mean)','Min. Score (SD)','Ratio vs. Fastest','Converged?'])
     for (n_islands, desc), group in replicates:
@@ -43,6 +45,9 @@ if __name__ == '__main__':
         ratio_vs_fastest = round(mean_hours / fastest_time, 2)
         converged = bool(mean_rounds < 999)
 
+        from scipy.stats import ttest_ind
+        tval,pval = ttest_ind(group['Hours'], fastest_hours)
+
         summary = summary.append({
             'Number of Islands': n_islands,
             'Connectivity': topology,
@@ -52,7 +57,8 @@ if __name__ == '__main__':
             'Min. Score (Mean)': mean_min_score,
             'Min. Score (SD)': std_min_score,
             'Ratio vs. Fastest': ratio_vs_fastest,
-            'Converged?': converged,
+            'Converged?': 'Yes' if converged else 'No',
+            'p-value (If Converged)': pval[0] if converged else None,
         }, ignore_index=True)
 
     print(summary)
