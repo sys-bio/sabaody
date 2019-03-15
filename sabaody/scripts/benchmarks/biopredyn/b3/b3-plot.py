b4815b661b9a48b0594cd74ae07a8d946aaafd34
0u812 <0u812@users.noreply.github.com>
2019-03-15 07:18:30 -0700

    update b3 plot and conv curves

--- sabaody/scripts/benchmarks/biopredyn/b3/b3-plot.py
+++ sabaody/scripts/benchmarks/biopredyn/b3/b3-plot.py
@@ -9,11 +9,17 @@ from b3problem import B3Problem
 from params import getDefaultParamValues, getBestKnownValues
 from data import measured_quantity_ids
 
+v = getBestKnownValues()
+# v[0] = 3.
+# v[10] = 3.
+# v[20] = 3.
+# for k in range(len(v)):
+#     v[k] = 3.
 # print(getDefaultParamValues())
 
 with open('../../../../../sbml/b3.xml', encoding='utf-8') as f:
     sbml = f.read()
 m = B3Problem(sbml)
-print(m.evaluate(getDefaultParamValues()))
+print(m.evaluate(v))
 for q in measured_quantity_ids:
-    m.plotQuantity(quantity_id=q, param_values=getDefaultParamValues())
+    m.plotQuantity(quantity_id=q, param_values=v)
