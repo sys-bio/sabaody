bff063f4362511d045ca1501a6dcc316435b4466
0u812 <0u812@users.noreply.github.com>
2019-03-23 15:27:46 -0700

    add more b4 run scripts

---
+++ sabaody/scripts/benchmarks/biopredyn/b4/run-scripts/stage3/run-8-r3-1.sh
@@ -0,0 +1,3 @@
+airflow test b4_benchmark b4_benchmark.b4.n_islands_8.Rim__bee_colony.r3 01-05-201
+airflow test b4_benchmark b4_benchmark.b4.n_islands_8.Rim__de.r3 01-05-201
+airflow test b4_benchmark b4_benchmark.b4.n_islands_8.Rim__de1220.r3 01-05-201
---
+++ sabaody/scripts/benchmarks/biopredyn/b4/run-scripts/stage3/run-8-r3-2.sh
@@ -0,0 +1,4 @@
+airflow test b4_benchmark b4_benchmark.b4.n_islands_8.Rim__de_nelder_mead.r3 01-05-201
+airflow test b4_benchmark b4_benchmark.b4.n_islands_8.Rim__de_praxis.r3 01-05-201
+airflow test b4_benchmark b4_benchmark.b4.n_islands_8.Rim__de_sade.r3 01-05-201
+airflow test b4_benchmark b4_benchmark.b4.n_islands_8.Rim__sade.r3 01-05-201
