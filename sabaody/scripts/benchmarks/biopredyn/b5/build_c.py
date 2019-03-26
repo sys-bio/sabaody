1ffaf5638fe2ac7f306bc974e9f48778bc8008af
0u812 <0u812@users.noreply.github.com>
2019-03-25 17:35:30 -0700

    add c builder to b5

---
+++ sabaody/scripts/benchmarks/biopredyn/b5/build_c.py
@@ -0,0 +1,47 @@
+experiment_num = 1
+
+import tempfile
+from os.path import join, dirname, realpath
+from json import load
+# from numpy import array
+
+from parameters import param_ids, param_id_to_default_value_map, base_param_id_to_index_map
+
+with open(join(dirname(realpath(__file__)), 'exp_y0.json')) as f:
+    exp_y0_collection = [a for a in load(f)]
+with open(join(dirname(realpath(__file__)), 'stimuli.json')) as f:
+    stimuli_collection = [d for d in load(f)]
+with open(join(dirname(realpath(__file__)), 'eval.c')) as f:
+    eval_code = f.read()
+
+lines = ''
+
+y0 = '{'+', '.join((str(x) for x in exp_y0_collection[experiment_num]))+'}'
+stimuli = stimuli_collection[experiment_num]
+
+lines += 'static double y[26] = '+y0+';\n'
+
+lines += '\n'
+
+lines += 'double egf = {};\n'.format(stimuli['egf'])
+lines += 'double tnfa = {};\n'.format(stimuli['tnfa'])
+lines += 'double pi3k_inh = {};\n'.format(stimuli['pi3k_inh'])
+lines += 'double raf1_inh = {};\n'.format(stimuli['raf1_inh'])
+
+lines += '\n'
+
+par_vals = [param_id_to_default_value_map[id] for id in param_ids]
+for k,v in enumerate(par_vals):
+    assert k == base_param_id_to_index_map[param_ids[k]]
+    assert v == param_id_to_default_value_map[param_ids[k]]
+pars = '{' + ',\n'.join((str(x) for x in par_vals))+'}'
+lines += 'pars = {};\n'.format(pars)
+
+lines += '\n'
+
+out_c = lines + eval_code
+print(out_c)
+
+with tempfile.NamedTemporaryFile(mode='w',suffix='.c') as f:
+    print(f.name)
+    f.write(out_c)
